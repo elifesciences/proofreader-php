571bf6a0c37111509e6725d9c53c069c10b7f2c6
Giorgio Sironi <g.sironi@elifesciences.org>
2016-07-18 12:02:41 +0100

    PHPMD-integrated test for ForbiddenClassRule

---
+++ test/samples/date_time_usage.php
@@ -0,0 +1,9 @@
+<?php
+
+function any_function_or_method()
+{
+    DateTime::createFromFormat('Y-m-d');
+    $object instanceof DateTime;
+    $object instanceof \DateTime;
+    return new DateTime();
+}
