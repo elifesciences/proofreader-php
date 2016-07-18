73924206b6e15bf6936e321ae22e768d6e8aed80
Giorgio Sironi <g.sironi@elifesciences.org>
2016-07-18 14:39:29 +0100

    Allowing override of default configuration files

--- README.md
+++ README.md
@@ -13,3 +13,38 @@ $ ./run.sh /path/to/some/src/folder
 ```
 
 The path passed in must be absolute.
+
+## Configuration
+
+By default, these configuration files are used:
+
+- `.php_cs.dist`
+- `ruleset.xml.dist`
+
+by PHP CS Fixer and PHPMD respectively.
+
+To override them you can create these files:
+
+- `.php_cs`
+- `ruleset.xml`
+
+## Sample output
+
+```bash
+$ proofreader-php/run.sh src/
+PHP-CS-Fixer consistency check with local project
+PHP-CS-Fixer
+Loaded config from "/home/giorgio/code/proofreader-php/.php_cs.dist"
+.........................
+Legend: ?-unknown, I-invalid file syntax, file ignored, .-no changes, F-fixed, E-error
+Checked all files in 1.792 seconds, 6.000 MB memory used
+PHPMD
+
+PHPCPD
+phpcpd 2.0.4 by Sebastian Bergmann.
+
+0.00% duplicated lines out of 725 total lines of code.
+
+Time: 43 ms, Memory: 4.00MB
+```
+
--- run.sh
+++ run.sh
@@ -4,14 +4,32 @@ set -e
 dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 code=$1
 
+
+
+function first_that_exists()
+{
+    list=$1
+    for file in $list
+    do
+        if [ -e "${dir}/${file}" ]       # Check if file exists.
+        then
+            echo "${dir}/${file}"
+            exit 0
+        fi
+    done 
+    exit 1
+}
+php_cs=$(first_that_exists ".php_cs .php_cs.dist")
+ruleset_xml=$(first_that_exists "ruleset.xml ruleset.xml.dist")
+
 echo "PHP-CS-Fixer consistency check with local project"
-$dir/bin/php-cs-fixer-consistency-check $dir/.php_cs.dist ./.php_cs
+$dir/bin/php-cs-fixer-consistency-check $php_cs ./.php_cs
 
 echo "PHP-CS-Fixer"
 $dir/vendor/bin/php-cs-fixer -vv fix $code --dry-run --config-file $dir/.php_cs.dist
 
 echo "PHPMD"
-$dir/vendor/bin/phpmd $code text $dir/ruleset.xml.dist | grep -v "Unexpected token: "
+$dir/vendor/bin/phpmd $code text $ruleset_xml | grep -v "Unexpected token: "
 
 echo "PHPCPD"
 $dir/vendor/bin/phpcpd $code
