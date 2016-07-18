7807604068565881ff6a92c46f7f6a48ee14ccd4
Giorgio Sironi <g.sironi@elifesciences.org>
2016-07-18 13:41:49 +0100

    Avoiding warnings on PHP 7 unparseable code
    ?? and 'use function' are statements that the PHPMD/PDepend is not able to parse yet

--- run.sh
+++ run.sh
@@ -3,5 +3,5 @@ set -e
 # switch to this working directory for simplicity
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-$DIR/vendor/bin/phpmd $* text $DIR/ruleset.xml.dist
+$DIR/vendor/bin/phpmd $* text $DIR/ruleset.xml.dist | grep -v "Unexpected token: "
 $DIR/vendor/bin/phpcpd $*
